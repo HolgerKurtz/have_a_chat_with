52815b4d120017e95f0bbdd4bec550d3a379231f
holgerkurtz <hk@holgerkurtz.de>
2023-02-12 20:08:10 +0100

    working gunther

--- telegram_artist_ai.py
+++ telegram_artist_ai.py
@@ -5,6 +5,7 @@ import html
 import traceback
 from functools import wraps
 from dotenv import load_dotenv
+import random
 
 from telegram import Bot, ChatAction, Update, ParseMode
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, CallbackContext
@@ -18,10 +19,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-
-
-with open('telegram_creds.json', "r") as json_file:
-    telegram_creds = json.load(json_file)
 
 TOKEN = os.getenv("Token")
 DEVELOPER_CHAT_ID = os.getenv("DEVELOPER_CHAT_ID")
@@ -80,19 +77,52 @@ dispatcher.add_error_handler(error_handler)
 
 
 def start(update, context):
-    update.message.reply_text("Wuff")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-def ai_image(update, context):
-    update.message.reply_text("Wuff")
-    
+    update.message.reply_text(random_wuffs())
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 @send_action(ChatAction.TYPING)
+def dog_description(update, context):
+    """ ASK FOR A DESCRIPTION """
+    chat_id = update.effective_chat.id
+    if str(update.message.text).lower().strip() == '/dog':
+        update.message.reply_text(
+            f"What does your dog look like?: /dog brown dachshund")
+    else:
+        description_list = update.message.text.split(' ')[1:]
+        description = ' '.join(description_list)
+        context.chat_data["dog_description"] = description
+        update.message.reply_text(
+            f"Done. This is how your dog looks like: {context.chat_data.get('dog_description')}")
+        update.message.reply_text("(You can change it.)")
+
+
+dog_description_handler = CommandHandler('dog', dog_description)
+dispatcher.add_handler(dog_description_handler)
+
+@send_action(ChatAction.TYPING)
+def image_generation(update, context):
+    chat_id = update.effective_chat.id
+    if str(update.message.text).lower().strip() == '/image':
+        update.message.reply_text(f"Describe the image to me: /image brown dachshund is playing with a british shorthair cat.")
+    else:
+        description_list = update.message.text.split(' ')[1:]
+        description = ' '.join(description_list)
+        update.message.reply_text(
+            f"Ok. That's what your image will look like: {description} // {context.chat_data.get('dog_description')}")
+        image_url = AIimage.generate_image(description)
+        context.bot.sendPhoto(chat_id=chat_id, photo=image_url, caption=description)
+
+image_generation_handler = CommandHandler('image', image_generation)
+dispatcher.add_handler(image_generation_handler)
+
+@send_action(ChatAction.TYPING)
 def wuff(update, context):
-    update.message.reply_text("Wuff")
+    if random.choice([True, False]):
+        update.message.reply_text(random_wuffs())
+    else:
+        update.message.reply_audio(audio=open(get_sound("sounds/"), 'rb'))
+
 wuff_handler = MessageHandler(Filters.text & (~Filters.command), wuff)
 dispatcher.add_handler(wuff_handler)
 
@@ -103,8 +133,22 @@ def unknown(update, context):
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
+
+def random_wuffs():
+    list_of_wuffs = []
+    for i in range(random.randrange(1,5)):
+        list_of_wuffs.append("Wuff")
+    return " ".join(list_of_wuffs)
+
+def get_sound(dir_path):
+    number_of_sounds = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))]) # https://pynative.com/python-count-number-of-files-in-a-directory/
+    bark_nr = random.randint(1,number_of_sounds+1)
+    path = f"{dir_path}bark{bark_nr}.mp3"
+    return path
+
 if __name__ == "__main__":
     # Starting the bot
     # For stopping the bot press: ctrl + c
-    updater.start_polling()
-    updater.idle()
+    # updater.start_polling()
+    # updater.idle()
+    get_sound()
